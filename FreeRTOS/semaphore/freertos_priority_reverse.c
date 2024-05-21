114d4cbac8893c2fdcf71f79998fb4f7b454a02c
liusheng <liushenggcs@163.com>
2024-05-21 16:03:17 +0800

    4.3 FreeRTOS信号量-优先级解决-互斥信号量继承优先级

--- FreeRTOS/semaphore/freertos_priority_reverse.c
+++ FreeRTOS/semaphore/freertos_priority_reverse.c
@@ -6,7 +6,8 @@
 #include "stm32f1xx_hal.h"
 
 #define TASK_STACk_SIZE 128
-SemaphoreHandle_t semaphoreBinary;
+SemaphoreHandle_t semaphoreBinary;  // 二值信号量
+SemaphoreHandle_t semaphoreMutex;   // 互斥信号量-可实现优先级继承
 static TaskHandle_t taskAHandle;
 static TaskHandle_t taskBHandle;
 static TaskHandle_t taskCHandle;
@@ -14,6 +15,7 @@ static TaskHandle_t taskCHandle;
 static void taskAFun(void *args) {
     osDelay(2000); // A优先级最高，但让出CPU,2秒后再抢占执行
     printf("taskA start running\r\n");
+    #if 0
     if(xSemaphoreTake(semaphoreBinary,portMAX_DELAY) != pdTRUE) {
         printf("taskA xSemaphoreTake error\r\n");
         return;
@@ -24,7 +26,17 @@ static void taskAFun(void *args) {
         printf("taskA xSemaphoreGive error\r\n");
         return;
     }
-
+    #endif
+    if(xSemaphoreTake(semaphoreMutex,portMAX_DELAY) != pdTRUE) {
+        printf("taskA xSemaphoreTake error\r\n");
+        return;
+    }
+    printf("taskA take semaphore\r\n");
+    printf("taskA print\r\n");
+    if(xSemaphoreGive(semaphoreMutex) != pdTRUE) {
+        printf("taskA xSemaphoreGive error\r\n");
+        return;
+    }
     vTaskDelete(NULL);
 }
 
@@ -33,6 +45,7 @@ static void taskBFun(void *args) {
     printf("taskB start running\r\n");
 
     // 打印B时，使用信号量，先获取信号量，打印10次，再释放信号量
+    #if 0
     if(xSemaphoreTake(semaphoreBinary,portMAX_DELAY) != pdTRUE) {
         printf("taskB xSemaphoreTake error\r\n");
         return;
@@ -46,7 +59,20 @@ static void taskBFun(void *args) {
         printf("taskB xSemaphoreGive error\r\n");
         return;
     }
-
+    #endif
+    if(xSemaphoreTake(semaphoreMutex,portMAX_DELAY) != pdTRUE) {
+        printf("taskB xSemaphoreTake error\r\n");
+        return;
+    }
+    printf("taskB take semaphore\r\n");
+    for(i = 0; i < 10; i++) {
+        printf("taskB print\r\n");
+        HAL_Delay(1000); // 注意不能使用osDelay(会从就绪列表迁移到延时列表，阻塞，让出CPU执行权)
+    }
+    if(xSemaphoreGive(semaphoreMutex) != pdTRUE) {
+        printf("taskB xSemaphoreGive error\r\n");
+        return;
+    }
     vTaskDelete(NULL);
     
 }
@@ -65,6 +91,7 @@ static void taskCFun(void *args) {
 
 void freertos_priority_reverse_init(void) {
     /* 创建二值信号量-注意创建后默认为0，要先释放信号量 */
+    #if 0
     semaphoreBinary = xSemaphoreCreateBinary();
     if(!semaphoreBinary) {
         printf("xSemaphoreCreateBinary error\r\n");
@@ -74,6 +101,13 @@ void freertos_priority_reverse_init(void) {
         printf("xSemaphoreGive error\r\n");
         return;
     }
+    #endif
+    /* 互斥信号量，初始为1，无需先释放 */
+    semaphoreMutex = xSemaphoreCreateMutex();
+    if(!semaphoreMutex) {
+        printf("semaphoreMutex error\r\n"); return;
+    }
+   
     /* 创建三个任务,优先级A>C>B */
     if(xTaskCreate(taskAFun,"taskA",TASK_STACk_SIZE,NULL,
                 osPriorityNormal2,&taskAHandle) != pdPASS) {
